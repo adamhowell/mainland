7d15c49a2fbb2ee305a7c84a29fc4f246c01c3e0
Yaroslav <yluchenko513@gmail.com>
2023-05-03 19:17:18 +0300

    Update: DOM

--- src/render/template.js
+++ src/render/template.js
@@ -5,8 +5,6 @@ export const htmlTemplate = `<!DOCTYPE html>
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
   <meta charset="UTF-8" />
   <link rel="icon" type="image/x-icon" href="/assets/img/favicon.ico">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
   <title>{Title}</title>
 
   <script>
