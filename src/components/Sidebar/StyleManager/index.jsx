2c7fd304ac99dc21404c54b997b6ecf99bbf2f17
Yaroslav <yluchenko513@gmail.com>
2023-05-04 19:38:51 +0300

    New: Selected section

--- src/components/Sidebar/StyleManager/index.jsx
+++ src/components/Sidebar/StyleManager/index.jsx
@@ -1,8 +1,21 @@
 import React from "react";
-import styles from "./StyleManager.module.scss"
+import styles from "./StyleManager.module.scss";
+import { useSelector } from "react-redux";
 
 const StyleManager = () => {
-  return <div className={`${styles.root}`}>Style Manager</div>;
+  const { selectedSection } = useSelector((state) => state.data);
+
+  return (
+    <div className={`${styles.root} p-4`}>
+      {selectedSection && (
+        <>
+          {" "}
+          Selected: <span className="opacity-50">{selectedSection?.tagName}</span>{" "}
+          <span className="opacity-50">#{selectedSection?.id}</span>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default StyleManager;
